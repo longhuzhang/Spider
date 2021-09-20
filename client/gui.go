6772a9db00092af38b90735a1622d7c3a60f7d71
zhanglh <zhanglonghu@hbsafety.net>
2021-09-21 00:36:36 +0800

    项目结构调整

---
+++ client/gui.go
@@ -0,0 +1,173 @@
+package client
+
+import (
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+	"log"
+)
+
+var (
+	userId          *walk.TextEdit
+	timeText        *walk.TextEdit
+	runStopBtn      *walk.PushButton
+	pauseStopBtn    *walk.PushButton
+	pauseRecoverBtn *walk.PushButton
+)
+var Mw = new(MyMainWindow)
+
+func UserOperate() {
+	if err := (MainWindow{
+		AssignTo: &Mw.MainWindow,
+		Title:    "酷狗视频爬虫",
+		Layout:   VBox{MarginsZero: true},
+
+		Children: []Widget{
+			Composite{
+				MinSize: Size{Width: 720, Height: 200},
+				Layout:  HBox{},
+				Children: []Widget{
+					VSplitter{
+						MaxSize: Size{Width: 120, Height: 100},
+						Children: []Widget{
+							PushButton{
+								Text: "打榜动态",
+								OnClicked: func() {
+									Mw.dynamicList()
+								},
+							},
+						},
+					},
+					VSplitter{
+						MaxSize: Size{Width: 120, Height: 100},
+						Children: []Widget{
+							PushButton{
+								Text: "飙升榜",
+								OnClicked: func() {
+									Mw.soaringList()
+								},
+							},
+						},
+					},
+				},
+			},
+
+			VSplitter{
+				//ColumnSpan: 1,
+				//MaxSize:    Size{550, 10},
+				Children: []Widget{
+					Label{Text: "输入时间:"},
+					TextEdit{
+						AssignTo: &timeText,
+						//MaxSize:  Size{Width: 550, Height: 20},
+					},
+				},
+			},
+
+			Composite{
+				MinSize: Size{550, 100},
+				Layout:  HBox{},
+				Children: []Widget{
+					VSplitter{
+						MaxSize: Size{Width: 120, Height: 100},
+						Children: []Widget{
+							PushButton{
+								Text: "日榜",
+								OnClicked: func() {
+									Mw.dayList()
+								},
+							},
+						},
+					},
+					VSplitter{
+						MaxSize: Size{Width: 120, Height: 50},
+						Children: []Widget{
+							PushButton{
+								Text: "周榜",
+								OnClicked: func() {
+									Mw.mouthList()
+								},
+							},
+						},
+					},
+				},
+			},
+
+			VSplitter{
+				ColumnSpan: 1,
+				//MinSize:    Size{550, 100},
+				Children: []Widget{
+					Label{Text: "玩家ID:"},
+					TextEdit{
+						AssignTo: &userId,
+						//MaxSize:  Size{Width: 100, Height: 40},
+					},
+				},
+			},
+			Composite{
+				MinSize: Size{550, 100},
+				Layout:  HBox{},
+				Children: []Widget{
+					VSplitter{
+						MaxSize: Size{90, 50},
+						Children: []Widget{
+							PushButton{
+								Text:      "退出",
+								AssignTo:  &pauseRecoverBtn,
+								OnClicked: Mw.offlinePauseRecover,
+							},
+						},
+					},
+					VSplitter{
+						MaxSize: Size{90, 50},
+						Children: []Widget{
+							PushButton{
+								Text:      "暂停",
+								AssignTo:  &pauseStopBtn,
+								OnClicked: Mw.offlineStop,
+							},
+						},
+					},
+					VSplitter{
+						MaxSize: Size{90, 50},
+						Children: []Widget{
+							PushButton{
+								Text:      "开始运行",
+								AssignTo:  &runStopBtn,
+								OnClicked: Mw.getUserIdAndOperate,
+							},
+						},
+					},
+				},
+			},
+		},
+	}).Create(); err != nil {
+		log.Fatal(err)
+	}
+
+	timeText.SetSize(walk.Size{800, 40})
+	userId.SetSize(walk.Size{800, 40})
+
+	pushBtn, _ := walk.NewPushButton(Mw)
+	pushBtn.SetText("结速运行")
+	pushBtn.SetSize(walk.Size{90, 50})
+	pushBtn.Clicked().Attach(func() {
+		pushBtn.SetText("运行成功！")
+	})
+
+	//screenX := int(win.GetSystemMetrics(win.SM_CXSCREEN))
+	//screenY := int(win.GetSystemMetrics(win.SM_CYSCREEN))
+	//
+	//if err := Mw.SetBounds(walk.Rectangle{
+	//	X: (screenX - Mw.MainWindow.Width()) / 2,
+	//	Y: (screenY - Mw.MainWindow.Height()) / 2,
+	//}); err != nil {
+	//	log.Print(err)
+	//	return
+	//}
+
+	Mw.MainWindow.SetSize(walk.Size{1400, 800})
+
+	//pauseRecoverBtn.SetVisible(false)
+	Mw.MainWindow.SetVisible(true)
+	Mw.MainWindow.Run()
+}
---
+++ client/work.go
@@ -0,0 +1,145 @@
+package client
+
+import (
+	"Spider/parase_url"
+	"Spider/public"
+	"Spider/public/error_operate"
+	"fmt"
+	"github.com/lxn/walk"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+type MyMainWindow struct {
+	*walk.MainWindow
+	prevFilePath string
+	path         string
+}
+
+func (mw *MyMainWindow) getUserIdAndOperate() {
+	defer func() {
+		if err := recover(); err != nil {
+			errMsg := ""
+			switch errType := reflect.TypeOf(err); errType {
+			case reflect.TypeOf(error_operate.NoticeError{}):
+				errMsg = err.(error_operate.NoticeError).Error()
+			default:
+				errMsg = "未知错误"
+			}
+			userId.SetText(errMsg)
+		}
+	}()
+	users := userId.Text()
+
+	if users == "" {
+		//userId.SetText("开始运行")
+		return
+	}
+
+	if strings.Contains(users, "shoudaoshuju") {
+		panic(error_operate.NoticeError{"传入玩家编号出错"})
+	}
+
+	//userId.SetText("")
+
+	runStopBtn.SetText("暂停")
+
+	userIds := strings.Split(users, ",")
+
+	fmt.Println(userIds)
+	for _, userId := range userIds {
+		go urlWork(userId)
+	}
+}
+
+func urlWork(MyUserId string) {
+	defer func() {
+		if err := recover(); err != nil {
+			errMsg := ""
+			switch errType := reflect.TypeOf(err); errType {
+			case reflect.TypeOf(error_operate.NoticeError{}):
+				errMsg = err.(error_operate.NoticeError).Error()
+			default:
+				errMsg = "未知错误"
+			}
+			userId.SetText(errMsg)
+		}
+	}()
+	parase_url.ParseId(MyUserId)
+}
+
+func (mw *MyMainWindow) dynamicList() {
+	urls := make([]string, 0)
+	for i := 0; i < 4; i++ {
+		strNum := strconv.Itoa(i)
+		url := "https://fx.service.kugou.com/VServices/Video.OfflineVideoRankService.getDynamicRank/" + strNum + "/?jsonpcallback=jsonphttpsfxservicekugoucomVServicesVideoOfflineVideoRankServicegetDynamicRank" + strNum + "jsonpcallback"
+		urls = append(urls, url)
+	}
+	go parase_url.ParasListUrl(urls, "动态榜单")
+}
+
+func (mw *MyMainWindow) soaringList() {
+	urls := make([]string, 0)
+	for i := 0; i < 2; i++ {
+		strNum := strconv.Itoa(i)
+		url := "https://fx.service.kugou.com/VServices/Video.OfflineVideoService.getWeeklyRisingRank/" + strNum + "/?jsonpcallback=jsonphttpsfxservicekugoucomVServicesVideoOfflineVideoServicegetWeeklyRisingRank" + strNum + "jsonpcallback"
+		//https://fx.service.kugou.com/VServices/Video.OfflineVideoService.getWeeklyRisingRank/0/?jsonpcallback=jsonphttpsfxservicekugoucomVServicesVideoOfflineVideoServicegetWeeklyRisingRank0jsonpcallback
+		urls = append(urls, url)
+	}
+	go parase_url.ParasListUrl(urls, "飙升榜单")
+}
+
+func (mw *MyMainWindow) dayList() {
+	urls := make([]string, 0)
+	strTime := timeText.Text()
+	timeText.SetText(fmt.Sprintf("爬取日期为：%s的视频", strTime))
+	for i := 0; i < 2; i++ {
+		strNum := strconv.Itoa(i)
+		url := "https://fx.service.kugou.com/VServices/Video.OfflineVideoService.getDailyRank/" + strNum + "-" + strTime + "/?jsonpcallback=jsonphttpsfxservicekugoucomVServicesVideoOfflineVideoServicegetDailyRank" + strNum + strTime + "jsonpcallback"
+		urls = append(urls, url)
+	}
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				errMsg := ""
+				switch errType := reflect.TypeOf(err); errType {
+				case reflect.TypeOf(error_operate.NoticeError{}):
+					errMsg = err.(error_operate.NoticeError).Error()
+				default:
+					errMsg = "未知错误"
+				}
+				timeText.SetText(errMsg)
+			}
+		}()
+		parase_url.ParasListUrl(urls, "日榜")
+	}()
+}
+
+func (mw *MyMainWindow) mouthList() {
+	strTime := timeText.Text()
+	urls := make([]string, 0)
+	for i := 0; i < 2; i++ {
+		strNum := strconv.Itoa(i)
+		url := "https://fx.service.kugou.com/VServices/Video.OfflineVideoService.getMonthRank/" + strNum + "-" + strTime + "/?jsonpcallback=jsonphttpsfxservicekugoucomVServicesVideoOfflineVideoServicegetMonthRank" + strNum + strTime + "jsonpcallback"
+		urls = append(urls, url)
+	}
+	go parase_url.ParasListUrl(urls, "月榜")
+}
+
+func (mw *MyMainWindow) offlinePauseRecover() {
+	//users := userId.Text()
+	public.MyChannel <- 1
+	pauseRecoverBtn.SetText("退出成功")
+
+}
+func (mw *MyMainWindow) offlineStop() {
+	str := pauseStopBtn.Text()
+	if str == "暂停" {
+		public.MyChannel <- 2
+		pauseStopBtn.SetText("继续")
+	} else {
+		public.Cond.Signal()
+		pauseStopBtn.SetText("暂停")
+	}
+}
